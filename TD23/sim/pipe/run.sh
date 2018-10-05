062d63bf7dacba1d8e63854bb9e4f95a71df8b85
Achille <achille.nazaret@gmail.com>
2018-10-05 20:58:48 +0200

    TD3 - Optimize assembly code
    Speed-up code, using pipeline and loop unrolling

---
+++ TD23/sim/pipe/run.sh
@@ -0,0 +1,9 @@
+./check-len.pl < ncopy.yo
+make VERSION=full
+../misc/yis sdriver.yo
+./correctness.pl
+#(cd ../y86-code; make testpsim)
+#(cd ../ptest; make SIM=../pipe/psim )
+#(cd ../ptest; make SIM=../pipe/psim TFLAGS=-i)
+# ###./correctness.pl -p
+./benchmark.pl
