d329e8809d3d11bf1a4398bb5df9950a8ea982c5
anazaret <achille.nazaret@gmail.com>
2018-11-15 18:32:16 +0100

    Lab8 - Malloc simulation

---
+++ TD89-MallocLab/mm.c
@@ -0,0 +1,172 @@
+/*
+ * mm-naive.c - The fastest, least memory-efficient malloc package.
+ * 
+ * In this naive approach, a block is allocated by simply incrementing
+ * the brk pointer.  A block is pure payload. There are no headers or
+ * footers.  Blocks are never coalesced or reused. Realloc is
+ * implemented directly using mm_malloc and mm_free.
+ *
+ * NOTE TO STUDENTS: Replace this header comment with your own header
+ * comment that gives a high level description of your solution.
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <unistd.h>
+#include <string.h>
+
+#include "mm.h"
+#include "memlib.h"
+
+
+static void printblock(void *bp);
+
+/*********************************************************
+ * NOTE TO STUDENTS: Before you do anything else, please
+ * provide your team information in the following struct.
+ ********************************************************/
+team_t team = {
+    /* Team name */
+    "Les Beaux Gosses du 44",
+    /* First member's full name */
+    "Dracula",
+    /* First member's email address */
+    "dracula@transilvania.net",
+    /* Second member's full name (leave blank if none) */
+    "",
+    /* Second member's email address (leave blank if none) */
+    ""
+};
+
+/* single word (4) or double word (8) alignment */
+#define ALIGNMENT 8
+
+/* rounds up to the nearest multiple of ALIGNMENT */
+#define ALIGN(size) (((size) + (ALIGNMENT-1)) & ~0x7)
+
+
+#define SIZE_T_SIZE (ALIGN(sizeof(size_t)))
+#define HDRP(bp)       (void*)((char *)(bp) - SIZE_T_SIZE)
+#define FTRP(bp)       (void*)((char *)(bp) + GET_SIZE(bp)) //line:vm:mm:ftrp
+
+/* Read and write a word at address p */
+#define GET(p)       (*(unsigned int *)(p))            //line:vm:mm:get
+#define PUT(p, val)  (*(unsigned int *)(p) = (val))    //line:vm:mm:put
+
+
+
+/* Read the size and allocated fields of bloc at address p */
+#define GET_SIZE(p)  (GET(HDRP(p)) & ~0x7)                   
+#define GET_ALLOC(p) (GET(HDRP(p)) & 0x1)                    
+
+int* free_list_start = (int*)-1;
+/* 
+ * mm_init - initialize the malloc package.
+ */
+int mm_init(void)
+{
+	void* start_address = mem_heap_lo();
+	printblock((void*)((char*)start_address + SIZE_T_SIZE));
+	free_list_start = (void*) -1;
+    return 0;
+}
+
+/* 
+ * mm_malloc - Allocate a block by incrementing the brk pointer.
+ *     Always allocate a block whose size is a multiple of the alignment.
+ */
+void *mm_malloc(size_t size)
+{
+    int newsize = ALIGN(size + SIZE_T_SIZE + SIZE_T_SIZE);
+    void *p = mem_sbrk(newsize);
+    if (p == (void *)-1)
+	return NULL;
+    else {
+        *(size_t *)p = size;
+        *(size_t *)((char *)p + (newsize-SIZE_T_SIZE)) = size;
+        return (void *)((char *)p + SIZE_T_SIZE);
+    }
+}
+
+/*
+ * mm_free - Freeing a block adds it at the beginning of the free_list 
+ */
+void mm_free(void *ptr)
+{
+	// If free_list is empty, start it 
+	if (free_list_start == (int*)-1){
+		free_list_start = (int*) ptr;
+	}
+}
+
+
+
+
+/*
+ * mm_realloc - Implemented simply in terms of mm_malloc and mm_free
+ */
+void *mm_realloc(void *ptr, size_t size)
+{
+    void *oldptr = ptr;
+    void *newptr;
+    size_t copySize;
+    
+    newptr = mm_malloc(size);
+    if (newptr == NULL)
+      return NULL;
+    copySize = *(size_t *)((char *)oldptr - SIZE_T_SIZE);
+    if (size < copySize)
+      copySize = size;
+    memcpy(newptr, oldptr, copySize);
+    mm_free(oldptr);
+    return newptr;
+}
+
+
+int mm_check(){
+	return 1;
+}
+
+
+static void printblock(void *bp) 
+{
+    size_t hsize, halloc, fsize, falloc;
+
+    //checkheap(0);
+    hsize = GET_SIZE(HDRP(bp));
+    halloc = GET_ALLOC(HDRP(bp));  
+    fsize = GET_SIZE(FTRP(bp));
+    falloc = GET_ALLOC(FTRP(bp));  
+
+    printf("%p: header: [%p:%c] footer: [%p:%c]\n", bp, 
+	hsize, (halloc ? 'a' : 'f'), 
+	fsize, (falloc ? 'a' : 'f')); 
+}
+
+//static void *coalesce(void *bp);
+/*
+void checkheap(int verbose) 
+{
+    char *bp = heap_listp;
+
+    if (verbose)
+	printf("Heap (%p):\n", heap_listp);
+
+    if ((GET_SIZE(HDRP(heap_listp)) != DSIZE) || !GET_ALLOC(HDRP(heap_listp)))
+	printf("Bad prologue header\n");
+    checkblock(heap_listp);
+
+    for (bp = heap_listp; GET_SIZE(HDRP(bp)) > 0; bp = NEXT_BLKP(bp)) {
+	if (verbose) 
+	    printblock(bp);
+	checkblock(bp);
+    }
+
+    if (verbose)
+	printblock(bp);
+    if ((GET_SIZE(HDRP(bp)) != 0) || !(GET_ALLOC(HDRP(bp))))
+	printf("Bad epilogue header\n");
+}*/
+
+
+
